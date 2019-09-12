482fb65898018742aed849e5a20d7f467c7211c9
camila-espitia <camila.espitia@gmail.com>
2019-09-11 19:39:45 -0500

    version1

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var app = express();
-app.get('/', (req, res) => {
-  if (!req.query.nombre){
+app.get('/makers/:nombre', (req, res) => {
+  if (!req.params.nombre){
     res.send("<h1>Hola desconocido!</h1>");
-  }else{res.send("<h1>Hola " + req.query.nombre + "!</h1>");} 
+  }else{
+    var str = req.params.nombre;
+    str = str[0].toUpperCase() + str.substring(1);
+  
+     res.send("<h1>Hola " + str + "!</h1>");
+  } 
 });
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
