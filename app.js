e87a3b04112840a40c882dde0fbdd332479f6c12
camila-espitia <camila.espitia@gmail.com>
2019-09-30 19:27:24 -0500

    c

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+const PORT = process.env.PORT || 3000
 app.get('/makers/:nombre', (req, res) => {
   if (!req.params.nombre){
     res.send("<h1>Hola desconocido!</h1>");
@@ -11,4 +12,4 @@ app.get('/makers/:nombre', (req, res) => {
   } 
 });
 
-app.listen(3000, () => console.log('Listening on port 3000!'));
+app.listen(PORT, () => console.log('Listening on port 3000!'));
